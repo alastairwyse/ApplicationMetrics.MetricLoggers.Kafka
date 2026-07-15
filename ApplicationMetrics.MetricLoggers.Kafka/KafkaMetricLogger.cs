753c3ab860cd46da921b59ce8f00b47f27c2c9db
Alastair Wyse <alastair@alastairwyse.net>
2026-07-15 23:12:00 +1000

    Added shell of KafkaMetricLogger class, and updated README.md

---
+++ ApplicationMetrics.MetricLoggers.Kafka/KafkaMetricLogger.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Copyright 2026 Alastair Wyse (https://github.com/alastairwyse/ApplicationMetrics.MetricLoggers.Kafka/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace ApplicationMetrics.MetricLoggers.Kafka
+{
+    /// <summary>
+    /// Writes metric events to a Kafka broker.
+    /// </summary>
+    public class KafkaMetricLogger : MetricLoggerBuffer
+    {
+        /// <summary>
+        ///  Initialises a new instance of the ApplicationMetrics.MetricLoggers.Kafka.KafkaMetricLogger class.
+        /// </summary>
+        /// <param name="bufferProcessingStrategy">Object which implements a processing strategy for the buffers (queues).</param>
+        /// <param name="intervalMetricBaseTimeUnit">The base time unit to use to log interval metrics.</param>
+        /// <param name="intervalMetricChecking">Specifies whether an exception should be thrown if the correct order of interval metric logging is not followed (e.g. End() method called before Begin()).  Note that this parameter only has an effect when running in 'non-interleaved' mode.</param>
+        public KafkaMetricLogger(IBufferProcessingStrategy bufferProcessingStrategy, IntervalMetricBaseTimeUnit intervalMetricBaseTimeUnit, Boolean intervalMetricChecking)
+             : base(bufferProcessingStrategy, intervalMetricBaseTimeUnit, intervalMetricChecking)
+        {
+
+        }
+
+        /// <inheritdoc/>
+        protected override void ProcessAmountMetricEvents(Queue<AmountMetricEventInstance> amountMetricEvents)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        protected override void ProcessCountMetricEvents(Queue<CountMetricEventInstance> countMetricEvents)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        protected override void ProcessIntervalMetricEvents(Queue<Tuple<IntervalMetricEventInstance, Int64>> intervalMetricEventsAndDurations)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        protected override void ProcessStatusMetricEvents(Queue<StatusMetricEventInstance> statusMetricEvents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ An implementation of an [ApplicationMetrics](https://github.com/alastairwyse/App
 
 #### TODO
 * Option to set description blank to reduce space
+* Producer should send whole set of metrics to  broker in one hit (similar to SQL Server / Postgres logger bulk insert)
+* Decide what to do with producer idempotence setting (https://docs.confluent.io/platform/current/installation/configuration/producer-configs.html#enable-idempotence)
+* Allow config object to be passed into metric logger (but may have to override some settings where metric implementation stipulates a certain value)
+* TKey on producer/consumer should be null be default BUT should have an option to override both TKey type and implementation of have value of TKey is derived (likely by an Action&lt;MetricInstanceBase&gt;)
+* Put note in doco about overriding TKey... could result in uneven partitioning
+* And also put a caveat about null TKey causing metric to arrive out of order (however this happens already in MetricLoggerBase so not losing anything)
+* Possibly need to expose an Action&lt;ProducerBuilder&gt; to allow client config
+* Create a utility class which consumes from Kafka and writes to another IMetricLogger instance
